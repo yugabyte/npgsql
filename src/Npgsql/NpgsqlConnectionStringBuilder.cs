1da6cf2ad23ee2655d285fe407afc74e23584fcd
Sfurti-yb <78196231+Sfurti-yb@users.noreply.github.com>
2024-06-04 17:43:07 +0530

    Suppress unsupported sqls and make discard statements flag enabled (#3)
    * Suppress unsupported sqls and make discard statements flag enabled
    
    * Changes as per review comments
    
    * Resolved High security vulnerability for the package (https://github.com/advisories/GHSA-98g6-xh36-x2p7)
    
    * Preparing for release

--- src/Npgsql/NpgsqlConnectionStringBuilder.cs
+++ src/Npgsql/NpgsqlConnectionStringBuilder.cs
@@ -1412,6 +1412,82 @@ public sealed partial class NpgsqlConnectionStringBuilder : DbConnectionStringBu
     }
     ServerCompatibilityMode _serverCompatibilityMode;
 
+    /// <summary>
+    /// Enables running the discard sequences sql during reset.
+    /// </summary>
+    [Category("Compatibility")]
+    [Description("Enables running the discard sequences sql during reset")]
+    [DisplayName("Enable Discard Sequences")]
+    [DefaultValue(true)]
+    [NpgsqlConnectionStringProperty]
+    public bool EnableDiscardSequences
+    {
+        get => _enablediscardsequences;
+        set
+        {
+            _enablediscardsequences = value;
+            SetValue(nameof(EnableDiscardSequences), value);
+        }
+    }
+    bool _enablediscardsequences;
+
+    /// <summary>
+    /// Enables running the discard sequences sql during reset.
+    /// </summary>
+    [Category("Compatibility")]
+    [Description("Enables running the discard temp sql during reset")]
+    [DisplayName("Enable Discard Temp")]
+    [DefaultValue(true)]
+    [NpgsqlConnectionStringProperty]
+    public bool EnableDiscardTemp
+    {
+        get => _enablediscardtemp;
+        set
+        {
+            _enablediscardtemp = value;
+            SetValue(nameof(EnableDiscardTemp), value);
+        }
+    }
+    bool _enablediscardtemp;
+
+    /// <summary>
+    /// Enables running the discard sequences sql during reset.
+    /// </summary>
+    [Category("Compatibility")]
+    [Description("Enables running the Close All sql during reset")]
+    [DisplayName("Enable Close All")]
+    [DefaultValue(true)]
+    [NpgsqlConnectionStringProperty]
+    public bool EnableCloseAll
+    {
+        get => _enablecloseall;
+        set
+        {
+            _enablecloseall = value;
+            SetValue(nameof(EnableCloseAll), value);
+        }
+    }
+    bool _enablecloseall;
+
+    /// <summary>
+    /// Enables running the discard sequences sql during reset.
+    /// </summary>
+    [Category("Compatibility")]
+    [Description("Enables running the Discard All sql during reset")]
+    [DisplayName("Enable Discard All")]
+    [DefaultValue(true)]
+    [NpgsqlConnectionStringProperty]
+    public bool EnableDiscardAll
+    {
+        get => _enablediscardall;
+        set
+        {
+            _enablediscardall = value;
+            SetValue(nameof(EnableDiscardAll), value);
+        }
+    }
+    bool _enablediscardall;
+
     #endregion
 
     #region Properties - Obsolete
